4fec34be942a495716cec77836a749f26241770e
sto0osik <adrian.stosur@gmail.com>
2019-08-19 13:23:49 +0200

    first commit

--- venv/face_reco.py
+++ venv/face_reco.py
@@ -1,5 +1,14 @@
 import numpy as np
 import cv2
+import glob
+
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+objp = np.zeros((6 * 9, 3), np.float32)
+objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+
+objpoints = []
+imgpoints = []
 
 cv2.namedWindow("video preview")
 vc = cv2.VideoCapture(0)
@@ -13,15 +22,42 @@ else:
     rval = False
 
 while rval:
+
     cv2.imshow("preview", frame)
     rval, frame = vc.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     ret, corners = cv2.findChessboardCorners(frame, (9, 6), None)
-    cv2.drawChessboardCorners(frame, (9, 6), corners, ret)
-    print(ret)
-    #ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-    #np.savez("pose/webcam_calibration_ouput", ret=ret, mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
-    out.write(frame)
+
+
+
+    #if
+
+    if ret == True:
+
+        objpoints.append(objp)
+        corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        imgpoints.append(corners2)
+
+        cv2.drawChessboardCorners(frame, (9, 6), corners, ret)
+        print(ret)
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+        print("ret:", ret)
+
+        h, w = gray.shape[:2]
+        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+        dst = cv2.undistort(gray, mtx, dist, None, newcameramtx)
+        x, y, w, h = roi
+        dst = dst[y:y + h, x:x + w]
+
+        mean_error = 0
+        for i in range(len(objpoints)):
+            imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+            error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
+            mean_error += error
+
+        print("total error: ", mean_error / len(objpoints))
+
+        out.write(frame)
 
     key = cv2.waitKey(20)
     if key == 27:
